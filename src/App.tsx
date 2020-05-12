56641d886bcf21f9ac53cdd49723d17a9ac500e8
Pawel L <virtuozoo@o2.pl>
2020-05-12 22:17:12 +0200

    change animation type

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,9 @@ const App: React.FC = () => {
   const location = useLocation();
   // eslint-disable-next-line no-shadow
   const transitions = useTransition(location, (location) => location.pathname, {
-    from: { opacity: 0, transform: 'translate(100%,0)' },
-    enter: { opacity: 1, transform: 'translate(0%,0)' },
-    leave: { opacity: 0, transform: 'translate(-50%,0)' },
+    from: { transform: 'translate3d(10vw, 0, 0)' },
+    enter: { transform: 'translate3d(0, 0, 0)' },
+    leave: { opacity: 0, transform: 'translate3d(-10vw, 0, 0)' },
   });
 
   return (
