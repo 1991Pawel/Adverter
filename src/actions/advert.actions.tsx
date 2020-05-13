addd3e2a37db8f6134c9a781b4aab3dc5d86495b
Pawel L <virtuozoo@o2.pl>
2020-05-13 19:12:05 +0200

    import const value to action

--- src/actions/advert.actions.tsx
+++ src/actions/advert.actions.tsx
@@ -1,14 +1,14 @@
 import Advert from '../types/adverts';
-import { AppActions } from '../types/actions';
 import { Id } from '../types/id';
+import { AppActions, ADD_ADVERT, REMOVE_ADVERT } from '../types/actions';
 
 export const addAdvert = (payload: Advert): AppActions => ({
-  type: 'ADD_ADVERT',
+  type: ADD_ADVERT,
   payload,
 });
 
 export const RemoveAdvert = (id: Id): AppActions => ({
-  type: 'REMOVE_ADVERT',
+  type: REMOVE_ADVERT,
   id,
 });
 
