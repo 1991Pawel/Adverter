eab35906969cbd798617d392326befa2189ddf16
Pawel L <virtuozoo@o2.pl>
2020-05-14 14:43:00 +0200

    fix import problem

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,8 @@ import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './i18next';
-import Spinner from './components/Spinner/Spinner';
-// eslint-disable-next-line import/order
 import { BrowserRouter as Router } from 'react-router-dom';
+import Spinner from './components/Spinner/Spinner';
 
 const App = React.lazy(() => import('./App'));
 
