f371d3726a5a63b9c8ae1e585853061f7ed651b7
Pawel L <virtuozoo@o2.pl>
2020-05-17 17:52:41 +0200

    remove useless import

--- src/components/Tutorial/Tutorial.jsx
+++ src/components/Tutorial/Tutorial.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable jsx-a11y/media-has-caption */
 import React, { useState } from 'react';
-import video2 from '../../assets/tutorialgif.gif';
 import video from '../../assets/tutorial.mp4';
 import styled from './Tutorial.module.scss';
 import videoPoster from '../../assets/videoplaceholder.jpg';
